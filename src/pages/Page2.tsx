f84f39c451ecf21680fa20c00a28d1b0bb3d0caf
k-isaka <isk.rugby@gmail.com>
2023-05-02 14:56:45 +0900

    feat: マウスオーバーイベントを検知する処理を追加

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
+// 参考: https://qiita.com/FumioNonaka/items/4361d1cdf34ffb5a5338
 export const Page2 = () => {
-    return <h1>ページ2</h1>
-
+    const [text, setText] = useState("ページ2デフォルト表示");
+    const handleMouseOverEvent: React.PointerEventHandler<HTMLButtonElement> = (event): void => {
+        setText("マウスオーバーされました");
+    }
+    return <button onMouseOver={handleMouseOverEvent}>{text}</button>
 }
