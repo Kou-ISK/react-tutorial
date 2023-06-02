45a418aef8f5427e8c8fcab0a086549295d17dcb
k-isaka <isk.rugby@gmail.com>
2023-06-02 22:06:28 +0900

    feat: chartが動作するように修正

--- src/pages/ChartView.tsx
+++ src/pages/ChartView.tsx
@@ -3,18 +3,29 @@ import Chart from "react-apexcharts";
 
 const TIME_RANGE_IN_MILLISECONDS = 30 * 1000;
 const ADDING_DATA_INTERVAL_IN_MILLISECONDS = 1000;
-const ADDING_DATA_RATIO = 0.8;
+const ADDING_DATA_RATIO = 0.9;
+
+// https://zenn.dev/luvmini511/articles/6c6f69481c2d17
+type DataPoint = {
+    x: Date;
+    y: number;
+}
+
+type ChartData = {
+    name: string;
+    data: DataPoint[];
+}
 
 export const ChartView = () => {
     const nameList = ["a", "b", "c"];
-    const defaultDataList = nameList.map(name => ({
+    const defaultDataList = nameList.map((name) => ({
         name: name,
-        data: []
+        data: [],
     }));
-    const [dataList, setDataList] = React.useState(defaultDataList);
+    const [dataList, setDataList] = React.useState<ChartData[]>(defaultDataList);
 
     React.useEffect(() => {
-        const addDataRandomly = (data: any) => {
+        const addDataRandomly = (data: DataPoint[]) => {
             if (Math.random() < 1 - ADDING_DATA_RATIO) {
                 return data;
             }
@@ -22,63 +33,45 @@ export const ChartView = () => {
                 ...data,
                 {
                     x: new Date(),
-                    y: data.length * Math.random()
-                }
+                    y: data.length * Math.random(),
+                },
             ];
         };
         const interval = setInterval(() => {
-            setDataList(
-                dataList.map(val => {
-                    return {
-                        name: val.name,
-                        data: addDataRandomly(val.data)
-                    };
-                })
+            setDataList((dataList) =>
+                dataList.map((val) => ({
+                    name: val.name,
+                    data: addDataRandomly(val.data),
+                }))
             );
         }, ADDING_DATA_INTERVAL_IN_MILLISECONDS);
 
         return () => clearInterval(interval);
-    });
+    }, []);
 
-    return (
-        <div>
-            <Charts
-                dataList={dataList}
-                range={TIME_RANGE_IN_MILLISECONDS}
-            />
-        </div>
-    );
-};
-
-
-const Charts = (props: any) => {
     const options = {
         chart: {
             zoom: { enabled: false },
-            animations: {
-                easing: "linear",
-                dynamicAnimation: {
-                    speed: 500
-                }
-            }
         },
         tooltip: {
-            x: { format: "yyyy/MM/dd HH:mm:ss.f" }
+            x: { format: "yyyy/MM/dd HH:mm:ss.f" },
         },
         xaxis: {
-            type: "datetime",
-            range: props.range
+            type: "datetime" as "datetime",
+            range: TIME_RANGE_IN_MILLISECONDS,
+            title: { text: "Time" },
         },
         yaxis: {
             labels: {
-                formatter: (val: number) => val.toFixed(0)
+                formatter: (val: number) => val.toFixed(0),
             },
-            title: { text: "Value" }
-        }
+            title: { text: "Value" },
+        },
     };
-    return (<>
-        <Chart type="line" options={options} series={props.dataList} />
-    </>)
 
-}
-
+    return (
+        <div>
+            <Chart type="line" options={options} series={dataList} />
+        </div>
+    );
+};
