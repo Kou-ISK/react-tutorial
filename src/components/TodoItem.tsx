061af7ca282969d2f241bb32746f82a6a8904f9c
k-isaka <isk.rugby@gmail.com>
2023-06-05 22:24:14 +0900

    feat: TodoリストのUIを修正

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Todo } from "../types/Todo";
+import { Card, Button, CardHeader, CardContent, Typography } from "@mui/material";
 
 // 1つのTodo、内容と移動・削除ボタン
 export const TodoItem = ({ todo, toggleTodoListItemStatus, deleteTodoListItem }: { todo: Todo; toggleTodoListItemStatus: any; deleteTodoListItem: any }) => {
@@ -8,9 +9,13 @@ export const TodoItem = ({ todo, toggleTodoListItemStatus, deleteTodoListItem }:
     const handleDeleteTodoListItem = () => deleteTodoListItem(todo.id);
     return (
         <>
-            {todo.content}
-            <button onClick={handleToggleTodoListItemStatus}>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
-            <button onClick={handleDeleteTodoListItem}>削除</button>
+            <Card variant="outlined">
+                <CardContent>
+                    <Typography>{todo.content}</Typography>
+                    <Button onClick={handleToggleTodoListItemStatus}>{todo.done ? "未完了リストへ" : "完了リストへ"}</Button>
+                    <Button onClick={handleDeleteTodoListItem}>削除</Button>
+                </CardContent>
+            </Card>
         </>
     );
 };
